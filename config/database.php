ef701c48f52ccb28e447df998dd4a110dc557f96
BneLevo <levent.ozgn@eduge.ch>
2026-08-27 09:38:43 +0200

    structure of db

--- config/database.php
+++ config/database.php
@@ -1,32 +1,7 @@
 <?php
-    
-require CONFIG_PATH . 'databaseInfo.php';
 
- class Database {
- 
-  private static $db = null;
-
-  private static $option = [
-  
-      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-      
-  ];
-  
-  public static function connexion(){
-  
-            if (is_null(self::$db) || !self::$db){
-            
-                try{
-                    self::$db = new PDO ("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";port=" . DB_PORT . ";charset=utf8mb4", DB_USER, DB_PASSWORD, self::$option);
-                }
-                catch(PDOException $e)
-	              {
-                    trigger_error( "Erreur connexion :" . $e->getMessage() . "");
-                }
-                
-            }
-            
-            return self::$db;
-    }
-}
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'horaire-eleve');
+define('DB_USER', 'root');
+define('DB_PASSWORD', ''); 
+define('DB_PORT', 3306);
---
+++ connexion/db.php
@@ -0,0 +1,32 @@
+<?php
+    
+require CONFIG_PATH . 'database.php';
+
+ class Database {
+ 
+  private static $db = null;
+
+  private static $option = [
+  
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+      
+  ];
+  
+  public static function connexion(){
+  
+            if (is_null(self::$db) || !self::$db){
+            
+                try{
+                    self::$db = new PDO ("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";port=" . DB_PORT . ";charset=utf8mb4", DB_USER, DB_PASSWORD, self::$option);
+                }
+                catch(PDOException $e)
+	              {
+                    trigger_error( "Erreur connexion :" . $e->getMessage() . "");
+                }
+                
+            }
+            
+            return self::$db;
+    }
+}
