8ea11003bff9788eaa604d657e5de4afce50e905
BneLevo <levent.ozgn@eduge.ch>
2026-08-27 09:38:04 +0200

    added some constants

--- config/constant.php
+++ config/constant.php
@@ -2,4 +2,8 @@
 
 define('ROOT', __DIR__ . '/../');
 define('CONFIG_PATH', ROOT . 'config/');
+define('CONNEXION_PATH', ROOT . 'connexion/');
+define('FUNCTIONS_PATH', ROOT . 'functions/');
 define('INCLUDES_PATH', ROOT . 'includes/');
+define('PAGES_PATH', ROOT . 'pages/');
+define('SQL_PATH', ROOT . 'sql/');
