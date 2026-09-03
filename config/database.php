f109fcbae28793b6c3d1647af2ab5cd648769501
BneLevo <levent.ozgn@eduge.ch>
2026-09-03 10:24:39 +0200

    changed with the right password

--- config/database.php
+++ config/database.php
@@ -3,5 +3,5 @@
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'horaire-eleve');
 define('DB_USER', 'root');
-define('DB_PASSWORD', ''); 
+define('DB_PASSWORD', 'root'); 
 define('DB_PORT', 3306);
